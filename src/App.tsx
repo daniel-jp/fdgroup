3f4c3f0d6b0cc58d2c11331e773bb6ecb27ca56b
daniel-paulino <daniel@beamtel.ma>
2023-08-11 03:50:19 +0100

    performing the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,117 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+import {Box, color} from '@chakra-ui/react';
+import React, {lazy, Suspense, useEffect, useState} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import GridLoader from 'react-spinners/GridLoader';
+
+//import {createBrowserRouter, createRoutesFromElements, Route, Routes} from 'react-router-dom';
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const wait = (time: number) => new Promise(resolve => setTimeout(resolve, time));
+  useEffect(() => {
+    // Aguarde 100ms antes de definir isLoading como false após o primeiro carregamento
+    wait(1000).then(() => setIsLoading(false));
+  }, []);
+  const PAGES = {
+    HOME: lazy(() => {
+
+      return wait(1000).then(() => import('./pages/home')).finally(() => setIsLoading(false));
+    }),
+    ABOUT: lazy(() => {
+
+      return wait(1000).then(() => import('./pages/About')).finally(() => setIsLoading(false));
+    }),
+    PRODUCT: lazy(() => {
+
+      return wait(1000).then(() => import('./pages/Products')).finally(() => setIsLoading(false));
+    }),
+    DEVICE: lazy(() => {
+
+      return wait(1000).then(() => import('./pages/Devices')).finally(() => setIsLoading(false));
+    }),
+    SERVER: lazy(() => {
+
+      return wait(1000).then(() => import('./pages/Server')).finally(() => setIsLoading(false));
+    }),
+    PART: lazy(() => {
+
+      return wait(1000).then(() => import('./pages/Parts')).finally(() => setIsLoading(false));
+    }),
+    AUDIV: lazy(() => {
+
+      return wait(1000).then(() => import('./pages/Audiovisuel')).finally(() => setIsLoading(false));
+    }),
+    OTHER: lazy(() => {
+      return wait(1000).then(() => import('./pages/Others')).finally(() => setIsLoading(false));
+    }),
+    SERVICE: lazy(() => {
+
+      return wait(1000).then(() => import('./pages/Services')).finally(() => setIsLoading(false));
+    }),
+    CONTACT: lazy(() => {
+      return wait(1000).then(() => import('./pages/Contact')).finally(() => setIsLoading(false));
+    }),
+
+  };
+
+
+  return (
+
+    <div className='App'>
+      <Box bg={"gray.800"}>
+        <Suspense fallback={<div style={{justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)', color: 'white', fontSize: '24px'}}>Loading...</div>}>
+          {isLoading ? (
+            <Box display="flex"
+              justifyContent="center"
+              alignItems="center"
+              height="100vh">
+
+              <GridLoader color="#dd1a78"
+                loading={isLoading} size={12}
+                aria-label="Loading Spinner"
+                data-testid="loader" />
+            </Box>
+          ) : (
+
+            <Box>
+              <Router>
+                <Routes>
+                  <Route path="/" element={<PAGES.HOME />} />
+                  <Route path="/about" element={<PAGES.ABOUT />} />
+                  <Route path="/products" element={<PAGES.PRODUCT />} />
+                  <Route path="/products/server" element={<PAGES.SERVER />} />
+                  <Route path="/products/devices" element={<PAGES.DEVICE />} />
+                  <Route path="/products/parts" element={<PAGES.PART />} />
+                  <Route path="/products/audiovisuel" element={<PAGES.AUDIV />} />
+                  <Route path="/products/other" element={<PAGES.PART />} />
+                  <Route path="/services" element={<PAGES.SERVICE />} />
+                  <Route path="/contact" element={<PAGES.CONTACT />} />
+                  <Route path="*" element={<p> NOT FOUND</p>}></Route>
+                </Routes>
+              </Router>
+            </Box>
+          )}
+        </Suspense>
+      </Box>
+    </div>
+
+  );
+}
+export default App;
+
+
+
+
+
+/*
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import {Box} from '@chakra-ui/react';
-import React, {Suspense, lazy, useEffect, useRef, useState} from 'react';
+import React, {lazy, Suspense, useEffect, useRef, useState} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GridLoader from 'react-spinners/GridLoader';
 
@@ -45,7 +155,7 @@ function App() {
               loading={isLoading} size={12}
               aria-label="Loading Spinner"
               data-testid="loader" />
-                </Box>
+            </Box>
           ) : (
       
         <Box>
@@ -74,3 +184,6 @@ function App() {
 }
 
 export default App;
+*/
+
+
